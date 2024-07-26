13c0fea79808f92e1a90479630a0ae981b7988cd
euhean <ehernandeza02@gmail.com>
2024-07-26 17:30:26 +0200

    Z index changes

--- Main_project/scripts/inv_ui_slot.gd
+++ Main_project/scripts/inv_ui_slot.gd
@@ -4,7 +4,7 @@ extends Panel
 
 func update(item: InventoryItem):
 	if item:
-		item_visual.visible = false
-	else:
 		item_visual.visible = true
 		item_visual.texture = item.texture
+	else:
+		item_visual.visible = false
